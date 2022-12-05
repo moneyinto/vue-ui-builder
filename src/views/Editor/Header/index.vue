5747ee665014c83220bb1eb48dd99e67d6e60c00
moneyinto <woyaoqianjin@gmail.com>
2022-12-05 17:34:23 +0800

    增加预览弹框

--- src/views/Editor/Header/index.vue
+++ src/views/Editor/Header/index.vue
@@ -5,20 +5,29 @@
         </div>
 
         <div class="editor-btns">
+            <el-button type="primary" link @click="openPreview">预览</el-button>
             <el-button type="primary" link @click="openVueCode">导出Vue</el-button>
         </div>
 
         <CodeDialog v-model="showCode" @close="(showCode = false)" />
+
+        <PreviewDialog v-model="showPreview" />
     </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 import CodeDialog from "./codeDialog.vue";
+import PreviewDialog from "./previewDialog.vue";
 
 const showCode = ref(false);
 const openVueCode = () => {
     showCode.value = true;
+};
+
+const showPreview = ref(false);
+const openPreview = () => {
+    showPreview.value = true;
 };
 </script>
 
---
+++ src/views/Editor/Header/previewDialog.vue
@@ -0,0 +1,41 @@
+<template>
+    <el-dialog
+        class="lyx-pre-view"
+        title="预览"
+        fullscreen
+        destroy-on-close
+    >
+        <Preview :slide="slide" />
+    </el-dialog>
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+import { useStore } from "@/store";
+import Preview from "@/views/Preview/index.vue";
+
+const store = useStore();
+const slide = computed(() => {
+    return {
+        name: "test",
+        widgetList: store.widgetList
+    };
+});
+</script>
+
+<style lang="scss">
+.lyx-pre-view {
+    .el-dialog__body {
+        padding-top: 5px !important;
+    }
+
+    .codemirror-body {
+        margin-top: 20px;
+    }
+
+    &.is-fullscreen {
+        --el-dialog-margin-top: 0;
+        --el-dialog-border-radius: 0;
+    }
+}
+</style>
